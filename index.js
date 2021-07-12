e312feb6f9575216b43e071be4b4b3d1c88dce6f
Nancy Zaky <nancyzki@yahoo.com>
2021-07-11 21:42:33 -0700

    commit tree 8fd92f2de8e35210afba7bb8f45f97d2e9c26009

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(input) {
+  if (input <= 400) {
+    return "This one is on me!";
+  } else if (input > 2500) {
+    return "No can do.";
+  } else if (input > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(input) {
+  if (input === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(input) {
+  if (input === "generous") {
+    return "Thank you so much.";
+  } else if (input === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
